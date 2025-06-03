6d9843cbe2ca2634bb1b6e62a5bc94676303e5fa
gnuh204 <ignuh204@gmail.com>
2025-06-04 00:17:16 +0700

    Hung

--- src/main/java/com/example/qiuzweb/controller/client/QuizController.java
+++ src/main/java/com/example/qiuzweb/controller/client/QuizController.java
@@ -1,18 +1,25 @@
 package com.example.qiuzweb.controller.client;
 
 import java.security.Principal;
+import java.util.List;
+import com.example.qiuzweb.domain.Category;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.example.qiuzweb.service.CategoryService;
 import com.example.qiuzweb.service.QuizService;
 @Controller
 public class QuizController {
 
     private final QuizService quizService;
-    public QuizController(QuizService quizService) {
+    private final CategoryService categoryService;
+    public QuizController(QuizService quizService, CategoryService categoryService) {
+        this.categoryService = categoryService;
         this.quizService = quizService;
     }
 
@@ -22,7 +29,10 @@ public class QuizController {
     }
 
     @GetMapping("/CreateQuiz")
-    public String CrteateQuiz() {
+    public String CrteateQuiz(Model model) {
+        List<Category> categories = categoryService.allCategory(); 
+        System.out.println("Số lượng category: " + categories.size());
+        model.addAttribute("categories", categories);
         return "client/createQuizFragment";
     }
 
@@ -50,6 +60,16 @@ public class QuizController {
             Principal principal) {
         quizService.saveQuiz(title, description, categoryId, timeLimitMinutes, imageFile, principal);
         return "redirect:/quizzes/list";
+    }
+    @PostMapping("/quizzes/save")
+    public String saveQuiz(@RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("categoryId") Long categoryId,
+            @RequestParam("timeLimitMinutes") Integer timeLimitMinutes,
+            @RequestParam("imageFile") MultipartFile imageFile,
+            Principal principal) {
+        quizService.saveQuiz(title, description, categoryId, timeLimitMinutes, imageFile, principal);
+        return "redirect:/Library";
     }
 
 }
--- src/main/java/com/example/qiuzweb/service/QuizService.java
+++ src/main/java/com/example/qiuzweb/service/QuizService.java
@@ -6,20 +6,29 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.qiuzweb.domain.Category;
 import com.example.qiuzweb.domain.Quiz;
 import com.example.qiuzweb.domain.User;
+import com.example.qiuzweb.repository.CategoryRepository;
 import com.example.qiuzweb.repository.QuizRepository;
 import com.example.qiuzweb.repository.UserRepository;
+
+import java.io.IOException;
+import java.nio.file.*;
 
 @Service
 public class QuizService {
     private final QuizRepository quizRepository;
     private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
 
-    public QuizService(QuizRepository quizRepository, UserRepository userRepository) {
+    public QuizService(QuizRepository quizRepository, UserRepository userRepository,
+            CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
         this.quizRepository = quizRepository;
         this.userRepository = userRepository;
     }
+
     public void saveQuiz(String title,
             String description,
             Long categoryId,
@@ -31,18 +40,28 @@ public class QuizService {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("Không tìm thấy người dùng"));
 
-        // Category category = categoryRepository.findById(categoryId)
-        // .orElseThrow(() -> new RuntimeException("Không tìm thấy chủ đề"));
-        String imageUrl = null;
-        if (!imageFile.isEmpty()) {
-            imageUrl = "/images/quiz" + imageFile.getOriginalFilename(); // có thể upload thật nếu cần
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new RuntimeException("Không tìm thấy chủ đề"));
+        String imgurl = null;
+        try {
+            String fileName = imageFile.getOriginalFilename();
+            Path uploadPath = Paths.get("src/main/resources/static/images/");
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(imageFile.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+            imgurl = fileName;
+        } catch (IOException e) {
+            throw new RuntimeException("Lỗi khi lưu ảnh", e);
         }
+
         Quiz quiz = new Quiz();
         quiz.setTitle(title);
         quiz.setDescription(description);
-        // quiz.setCategory(category);
+        quiz.setCategory(category);
         quiz.setTimeLimitMinutes(timeLimitMinutes);
-        quiz.setImageUrl(imageUrl);
+        quiz.setImageUrl(imgurl);
         quiz.setCreatedAt(LocalDateTime.now());
         quiz.setCreatedBy(user);
         quizRepository.save(quiz);
