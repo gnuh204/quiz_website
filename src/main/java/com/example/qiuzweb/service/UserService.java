f44bf31afc2c2685b7885efa1a103ad658b9c165
Huy Nguyen <evsnoway2308@gmail.com>
2025-06-03 23:58:30 +0700

    huy

--- src/main/java/com/example/qiuzweb/service/UserService.java
+++ src/main/java/com/example/qiuzweb/service/UserService.java
@@ -11,45 +11,50 @@ import com.example.qiuzweb.repository.UserRepository;
 
 @Service
 public class UserService {
-    private final UserRepository userResitory;
+    private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userResitory , PasswordEncoder passwordEncoder) {
-        this.userResitory = userResitory;
-        this.passwordEncoder = passwordEncoder;
+public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
+}
+
+    public boolean usernameexists(String username) {
+        return userRepository.findByUsername(username).isPresent();
     }
 
-    public boolean usernameexists(String username){
-         return userResitory.findByUsername(username).isPresent();
+    public boolean emailexists(String email) {
+        return userRepository.findByEmail(email).isPresent();
     }
 
-    public boolean emailexists(String email){
-        return userResitory.findByEmail(email).isPresent();
-    }
-    
-    public User findByEmail(String email){
-        return this.userResitory.findByEmail(email).orElse(null);
+    public User findByEmail(String email) {
+        return this.userRepository.findByEmail(email).orElse(null);
     }
 
-    public User registerUser(UserDTO userDTO){
+    public User registerUser(UserDTO userDTO) {
         User user = new User();
         user.setUsername(userDTO.getUsername());
         user.setEmail(userDTO.getEmail());
         user.setPasswordHash(passwordEncoder.encode(userDTO.getPassword()));
         user.setRole(User.Role.USER);
-        return userResitory.save(user);
+        return userRepository.save(user);
     }
 
-public User registerUserAdmin(UserDTO userDTO){
+    public User registerUserAdmin(UserDTO userDTO) {
         User user = new User();
         user.setUsername(userDTO.getUsername());
         user.setEmail(userDTO.getEmail());
         user.setPasswordHash(passwordEncoder.encode(userDTO.getPassword()));
         user.setRole(userDTO.getRole());
-        return userResitory.save(user);
+        return userRepository.save(user);
     }
-  public List<User> findAllUser() {
-    return userResitory.findAll();
+
+    public List<User> findAllUser() {
+        return userRepository.findAll();
+    }
+
+public void deleteUserById(Long id) {
+    userRepository.deleteById(id);
 }
 
 }
